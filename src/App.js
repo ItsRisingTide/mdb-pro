e04acb0eb8751878b9442181398e75efcf8a579f
IisRisingTide <artur25rus2@mail.ru>
2021-03-29 15:12:53 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 
 function App() {
   return (
@@ -9,15 +10,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol>fdf1</MDBCol>
+          <MDBCol>fdf2</MDBCol>
+          <MDBCol>fdf3</MDBCol>
+          <MDBBtn>dfgdfg</MDBBtn>
+        </MDBRow>
+      </MDBContainer>
     </div>
   );
 }
